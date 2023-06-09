90c0d8058998c5d4b5d207c64cb536c556e1b0b5
kartikey4474 <kartikeysingh4474@gmailcom>
2023-06-10 01:20:58 +0530

    Initial commit

---
+++ lib/features/admin/widgets/category_products_chart.dart
@@ -0,0 +1,21 @@
+
+// import 'package:flutter/material.dart';
+// import 'package:syncfusion_flutter_charts/charts.dart';
+
+// import '../model/sales.dart';
+
+// class CategoryProductsChart extends StatelessWidget {
+//   final List<SfCartesianChart.Series<Sales, String>> seriesList;
+//   const CategoryProductsChart({
+//     Key? key,
+//     required this.seriesList,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SfCartesianChart.BarChart(
+//       seriesList,
+//       animate: true,
+//     );
+//   }
+// }
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:amazon_clone/constants.dart/global_variables.dart';
+import 'package:amazon_clone/provider/user_provider.dart';
+import 'package:amazon_clone/router.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'common/widgets/bottom_bar.dart';
+import 'features/admin/screens/admin_screen.dart';
+import 'features/auth/screens/auth_screen.dart';
+import 'features/auth/services/auth_service.dart';
+
+void main() {
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+    ),
+  ], child: const MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService authService = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Amazon Clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: Provider.of<UserProvider>(context).user.token.isNotEmpty
+          ? Provider.of<UserProvider>(context).user.type == 'user'
+              ? const AdminScreen()
+              : const AdminScreen()
+          : const AdminScreen(),
+    );
+  }
+}
